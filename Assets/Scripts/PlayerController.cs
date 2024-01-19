f1533cc50f7c82e0e5d7f0f0eb3681d5bcc639ff
alba-rm <alba.r.m.722@gmail.com>
2024-01-19 10:52:33 +0100

    navmesh

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _playerAgent;
+
+    void Awake()
+    {
+        _playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            SetDestination();
+        }
+    }
+    void SetDestination()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            _playerAgent.destination = hit.point;
+        }
+    }
+}
